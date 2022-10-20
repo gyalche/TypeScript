df9e137834f34e71ff42848f97400dfd5fd37a08
Dawa <sherpadawa01@outlook.com>
2022-10-20 17:04:25 +0545

    learn to use the types:

--- 07/index.ts
+++ 07/index.ts
@@ -1,1 +1,8 @@
-function calculateTotalPrice
+//remember when you use .toFixed(2) it will return string;
+function calculateTotalPrice(price: number): number {
+  return +(price + price * 0.18).toFixed(1);
+}
+
+const priceOfToy = calculateTotalPrice(1234);
+const discountedPrice = priceOfToy / 2;
+console.log(discountedPrice);
