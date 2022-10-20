8657b3dc3f573d98b8f07ab1ada8223d476bf429
Dawa <sherpadawa01@outlook.com>
2022-10-20 15:57:11 +0545

    type:string, number

--- index.ts
+++ index.ts
@@ -1,10 +1,42 @@
-let haha = 'dawa';
+// let haha = 'dawa';
 
-console.log(typeof haha);
-console.log(haha);
+// console.log(typeof haha);
+// console.log(haha);
 
-function sum(a: number, b: number): number {
-  return a + b;
-}
+// function sum(a: number, b: number): number {
+//   return a + b;
+// }
 
-console.log(sum(10, 10));
+// console.log(sum(10, 10));
+
+//PIMITIVE TYPES;
+//string;
+let myName: string = 'Dawa';
+
+//number;
+let myAge: number = 25;
+
+//boolean;
+let isEnable: boolean;
+isEnable = true;
+
+//null;
+let Married: null;
+
+//undefined;
+//symbol;
+
+//OBJECT TYPES;
+//array, object, classes
+
+//Types for Array;
+let guest: string[] = ['joe', 'jeni', 'john'];
+
+//Types for function;
+// let killAm: (name: string) => string;
+
+const killAm = function (name: string): string {
+  return name.toUpperCase();
+};
+
+console.log(killAm('dawa'));
---
+++ out/index.js
@@ -0,0 +1,7 @@
+var haha = 'dawa';
+console.log(typeof haha);
+console.log(haha);
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(10, 10));
