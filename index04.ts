95f9439983143dbe0ec6bd2987e551141b4ed8d3
Dawa <sherpadawa01@outlook.com>
2022-10-20 16:06:45 +0545

    type inference

---
+++ index04.ts
@@ -0,0 +1,10 @@
+let nam = 'Dawa';
+console.log(typeof nam);
+
+let age = 2022;
+console.log(typeof age);
+
+let guests = ['dawa', 'kancha', 'tika', 22, true];
+console.log(typeof guests);
+
+//type Inference=>TS automatically knows the type
