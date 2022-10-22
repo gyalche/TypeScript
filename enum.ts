ed8c1a5a323f6c82defea6fafc6146c0ca8906aa
Dawa <sherpadawa01@outlook.com>
2022-10-22 17:37:49 +0545

    learned enum

---
+++ enum.ts
@@ -0,0 +1,14 @@
+//ENUM type;
+//javascript doesnt support enum;
+
+enum Weekday {
+  sunday,
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+}
+
+console.log(Weekday.monday);
