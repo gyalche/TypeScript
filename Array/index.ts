406648a349cea4538eee78f22377f2fb910a51a1
Dawa <sherpadawa01@outlook.com>
2022-10-25 14:12:53 +0545

    array and tuple

---
+++ Array/index.ts
@@ -0,0 +1,13 @@
+//Array and Tuple type;
+
+//tuple is provided in typescript
+//it is just like array;
+
+//array;
+let fruits: string[];
+fruits = ['mango', 'banana', 'apple', 'pineapple'];
+
+//tuple;
+let color: [number, number, number] = [244, 244, 0];
+// color = [...color, 255];
+console.log(color);
