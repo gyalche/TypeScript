4c9f1a43caa5c0de19ab36a33b0e64f088f67e0c
Dawa <sherpadawa01@outlook.com>
2022-10-25 13:58:03 +0545

    object

---
+++ 09/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// let fruit: object;
+let fruit;
+fruit = {
+    name: 'Apple',
+    taste: 'Sweet',
+    color: 'yellow',
+    allSeason: false,
+};
+console.log(typeof fruit.allSeason);
---
+++ 09/index.ts
@@ -0,0 +1,22 @@
+// let fruit: object;
+
+// let fruit: {
+//   name: string;
+//   taste: string;
+//   color: string;
+//   allSeason: boolean;
+// };
+
+let fruit: {
+  name: string;
+  taste: string;
+  color: string;
+  allSeason: boolean;
+} = {
+  name: 'Apple',
+  taste: 'Sweet',
+  color: 'yellow',
+  allSeason: false,
+};
+
+console.log(typeof fruit.allSeason);
