2f9a684d3d2aca57f8c7e75495c73867e94043d6
Dawa <sherpadawa01@outlook.com>
2022-10-26 15:48:08 +0545

    generice constrain

--- generics/index.ts
+++ generics/index.ts
@@ -2,17 +2,27 @@
 //why we need generics;
 //=> genreic means a thing that can be used in difference scenario and adapt to that scenario;
 
+// let arrNum = [23, 4356, 6, 7, 8, 8, 34];
+// let arrString = ['a', 'b', 'c', 'd', 'e'];
+// //<T> Generic type</T>
+// function getRandomElement<T>(arr: T[]): T {
+//   const index = Math.floor(Math.random() * arr.length);
+//   return arr[index];
+// }
 
-let arrNum = [23, 4356, 6, 7, 8, 8, 34];
-let arrString = ['a', 'b', 'c', 'd', 'e'];
-//<T> Generic type</T>
-function getRandomElement<T>(arr: T[]): T {
-  const index = Math.floor(Math.random() * arr.length);
-  return arr[index];
-}
+// let result: string;
 
-let result: string;
+// result = getRandomElement(arrString);
+// getRandomElement(arrNum);
+// console.log(typeof result);
 
-result = getRandomElement(arrString);
-getRandomElement(arrNum);
-console.log(typeof result);
+//GENERIC CONSTRAINT;
+type Person = {
+  firstname: string;
+};
+
+// function getPerson<T extends Person>(data: T) {
+//   return data;
+// }
+const details = { lastname: 'Sherpa', firstname: 'dawa' };
+// console.log(getPerson(details));
